0aa5f02cfe57c930362b7accfbdec0d55ef6ded5
Tim Niemueller <tim@niemueller.de>
2023-07-22 10:53:45 +0200

    simple solution pin configuration

--- solution_simple/solution_simple.ino
+++ solution_simple/solution_simple.ino
@@ -3,6 +3,15 @@
 void setup() {
   // Serial setup (optional, can be used for debugging output)
   Serial.begin(115200);
+
+  // input is with pullup resistor, therefore "LOW" on that pin is signal
+  pinMode(PIN_OUT, OUTPUT);
+  // use builtin LED to visualize what is sent
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  // signal HIGH and turn off LED initially
+  digitalWrite(PIN_OUT, HIGH);
+  digitalWrite(LED_BUILTIN, LOW);
 }
 
 void loop() {
